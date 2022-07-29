6840f3bd1624a29d0e604d4d347f0a4ca313b385
mibook <3072554288@qq.com>
2022-07-29 15:44:45 +0800

    -Fixed the bug that the logfile created in a wrong path.

--- EachTech.SoftwareMgr.RuntimeInstaller/Log.cs
+++ EachTech.SoftwareMgr.RuntimeInstaller/Log.cs
@@ -23,7 +23,7 @@ namespace RuntimeInstaller
                 //空格是为了增强日志可读性,DateTime的作用是获取目前时间
                 Directory.CreateDirectory($"{Path}/Log/");
                 //如果不存在Log文件夹,则创建(会略微拖慢运行速度,但是用if判断一次代码量和工作量会大很多)
-                File.AppendAllText($"{Path}/Console" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", "\r\n" + message);
+                File.AppendAllText($"{Path}/Log/Console" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", "\r\n" + message);
                 //AppendAllText是追加到文件末尾.因为文件名不能出现"/",所以这里在ToString里面指定格式为yyyy-MM-dd.
                 //为了使文件便于查找,因此一天一个文件
                 Console.WriteLine(message);
@@ -50,7 +50,7 @@ namespace RuntimeInstaller
                 //空格是为了增强日志可读性,DateTime的作用是获取目前时间
                 Directory.CreateDirectory($"{Path}/Log/");
                 //如果不存在Log文件夹,则创建(会略微拖慢运行速度,但是用if判断一次代码量和工作量会大很多)
-                File.AppendAllText($"{Path}/Console" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", $"\r\n" + message);
+                File.AppendAllText($"{Path}/Log/Console" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", $"\r\n" + message);
                 //AppendAllText是追加到文件末尾.因为文件名不能出现"/",所以这里在ToString里面指定格式为yyyy-MM-dd.
                 //为了使文件便于查找,因此一天一个文件
                 Console.WriteLine($"{message}");
@@ -77,7 +77,7 @@ namespace RuntimeInstaller
                 //空格是为了增强日志可读性,DateTime的作用是获取目前时间
                 Directory.CreateDirectory($"{Path}/Log/");
                 //如果不存在Log文件夹,则创建(会略微拖慢运行速度,但是用if判断一次代码量和工作量会大很多)
-                File.AppendAllText($"{Path}/Console" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", "\r\n" + message);
+                File.AppendAllText($"{Path}/Log/Console" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", "\r\n" + message);
                 //AppendAllText是追加到文件末尾.因为文件名不能出现"/",所以这里在ToString里面指定格式为yyyy-MM-dd.
                 //为了使文件便于查找,因此一天一个文件
                 if (output) 
@@ -107,7 +107,7 @@ namespace RuntimeInstaller
                 //空格是为了增强日志可读性,DateTime的作用是获取目前时间
                 Directory.CreateDirectory($"{Path}/Log/");
                 //如果不存在Log文件夹,则创建(会略微拖慢运行速度,但是用if判断一次代码量和工作量会大很多)
-                File.AppendAllText($"{Path}/Console" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", $"\r\n " + message);
+                File.AppendAllText($"{Path}/Log/Console" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", $"\r\n " + message);
                 //AppendAllText是追加到文件末尾.因为文件名不能出现"/",所以这里在ToString里面指定格式为yyyy-MM-dd.
                 //为了使文件便于查找,因此一天一个文件
                 if (output)
